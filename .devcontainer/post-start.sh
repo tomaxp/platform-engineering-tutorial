e1ff68dec3cf43e8bef93543513750daab4ac49e
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2023-10-04 11:46:29 +1000

    update
    Signed-off-by: Adam Gardner <26523841+agardnerIT@users.noreply.github.com>

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -2,6 +2,15 @@
 
 echo "post-start start" >> ~/status
 
+echo "Creating kind cluster..."
+kind create cluster
+
+echo "Created kind cluster"
+echo "Applying nginx demo app"
+kubectl apply -f .devcontainer/nginxdemo.yml
+echo "Applying ArgoCD nodeport"
+kubectl apply -f .devcontainer/argocd-nodeport.yaml
+
 # this runs in background each time the container starts
 
 echo "post-start complete" >> ~/status
