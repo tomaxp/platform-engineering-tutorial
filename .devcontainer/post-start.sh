40226cc5758763b806596fe476e4418324cd55c1
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2023-10-04 06:27:37 +0000

    update
    Signed-off-by: Adam Gardner <26523841+agardnerIT@users.noreply.github.com>

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -4,8 +4,8 @@ USERNAME=vscode
 
 echo "post-start start" >> /home/$USERNAME/status
 
-echo "Creating kind cluster..."
-kind create cluster
+#echo "Creating kind cluster..."
+#kind create cluster
 
 echo "Created kind cluster"
 
@@ -13,8 +13,15 @@ echo "Deploy ArgoCD"
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-echo "Wait for ArgoCD to be ready..."
-kubectl wait --for=condition=available deployment/argocd-server -n argocd --timeout=300s
+#echo "Wait for ArgoCD to be ready..."
+#kubectl wait --for=condition=available deployment/argocd-server -n argocd --timeout=300s
+
+echo "Configuring ArgoCD for no TLS"
+kubectl -n argocd apply -f .devcontainer/argocd-no-tls.yaml
+
+echo "Restarting ArgoCD server to pick up TLS changes"
+kubectl -n argocd scale deploy/argocd-server --replicas=0
+kubectl -n argocd scale deploy/argocd-server --replicas=1
 
 kubectl -n argocd rollout status deploy/argocd-server --timeout=300s
 
